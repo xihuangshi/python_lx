790fb998fe75685dd6880691a553f90c394a08f8
xihuangshi <huameitangsanzhcg@163.com>
2025-02-20 00:04:53 +0800

    feat: add 变量和类型 initial project

---
+++ 2.变量和类型/variable.py
@@ -0,0 +1,17 @@
+a = 1
+b = 1.0
+c = "我是1"
+d = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+e = float(a)
+f = int(b)
+g = bool(d)#非空字符串转bool都是Ture
+h = str(d)
+print(e)
+print(f)
+print(g)
+print(h)
